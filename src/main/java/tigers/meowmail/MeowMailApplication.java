ff3757febf5aaa060d298e5ae8b9ef090603b941
yeonise <yeonisekim@naver.com>
2026-01-11 16:31:01 +0900

    init project

---
+++ src/main/java/tigers/meowmail/MeowMailApplication.java
@@ -0,0 +1,13 @@
+package tigers.meowmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tigers/meowmail/MeowMailApplicationTests.java
@@ -0,0 +1,13 @@
+package tigers.meowmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeowMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
