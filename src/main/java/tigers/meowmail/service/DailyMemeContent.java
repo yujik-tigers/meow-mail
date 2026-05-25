13266548c43b71e6c529e8c45291c3c846680f1a
hyun <ghkdgus29@naver.com>
2026-05-25 19:52:58 +0900

    modify: change DailyMemeContent and template rendering for changed get content spec

--- src/main/java/tigers/meowmail/service/DailyMemeContent.java
+++ src/main/java/tigers/meowmail/service/DailyMemeContent.java
@@ -5,12 +5,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DailyMemeContent(
+	String type,
+
 	@JsonProperty("image_url")
 	String imageUrl,
-	String expressions,
-	String translation,
-	String background,
+
+	String content,
+	@JsonProperty("content_translation")
+	String contentTranslation,
+
+	String expression,
+	@JsonProperty("expression_translation")
+	String expressionTranslation,
+
 	String author,
-	String source) {
+	) 
+	{
 
 }
